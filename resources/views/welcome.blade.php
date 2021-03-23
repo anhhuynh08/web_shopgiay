6061c14643b5689d6407cc058c64ea7b53ab8b3d
huynhanh98 <anhtititi@gmail.com>
2021-03-23 15:07:34 +0700

    test

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop giay</title>
+    <link href="fontawesome/css/all.css" rel="stylesheet" >
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" >
+    <link href="public/frontend/css/customs.css" rel="stylesheet" >
+</head>
+<body>
+   <div class="container-fluid">
+       <div class="header-top">
+           <div class="row">
+               <div class="logo">
+                   <h3 class="logo">LOGO|LOGO</h3>
+               </div>
+           </div>
+       </div>
+       <div class="header-mid">
+        <div id="menu">
+                <ul>
+                    <li><a href="#">FEATURES</a></li>
+                    <li><a href="#">FOOTWEAR</a></li>
+                    <li><a href="#">APPAREL</a>
+                        <ul class="sub-menu">
+                                <li><a href="#">Áo</a></li>
+                                <li><a href="#">WordPress</a></li>
+                                <li><a href="#">WordPress</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">ACCESSORIES</a></li>
+                    <li><a href="#">USED</a></li>
+                </ul>
+        </div>
+           
+       </div>
+   </div>
+</body>
+<script src="bootstrap/js/jquery-1.7.1-vsdoc.js" ></script>
+<script src="bootstrap/js/bootstrap.min.js" ></script>
+</html>
